cf9410314d9ff5ee85dad37b9808c2615aa88c06
League Student <league@iMac-28.attlocal.net>
2019-05-15 18:25:00 -0700

    some initial work

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,20 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	public static final int WIDTH=500;
+	public static final int HEIGHT=800;
+JFrame frame=new JFrame();
+	public static void main(String[] args) {
+		LeagueInvaders l=new LeagueInvaders();
+		l.setup();
+}
+	public void setup() {
+		Dimension d=new Dimension(WIDTH,HEIGHT);
+		frame.setPreferredSize(d);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setVisible(true);
+	frame.pack();
+	}
 }
